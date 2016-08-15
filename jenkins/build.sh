a6abcf024fd90d8a604b3ce1297792d7de5996d5
Josh Brown <jbrown@sightmachine.com>
2016-08-15 13:43:54 -0700

    Updating with Jenkins specific files

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -10,16 +10,30 @@ set -e
 ##   1. Removes any existing rendered documentation (gitbooks)
 ##   2. Uses docker to render/build the book
 ##   3. Creates a tar.gz of the rendered book
-##   4. Pushes that tar.gz to S3
 ##
 _SCRIPT_DIR=$(dirname $0)
 _GIT_SHA=$(git rev-parse HEAD)
 
+function _create_temporary_dirs() {
+  test -d artifacts && rm -rf artifacts
+  test -d _book && rm -rf _book
+}
+
+function _build() {
+  sudo docker-compose run --rm gitbook gitbook build
+  sudo chown -R jenkins:jenkins _book #OMG
+}
+
+function _archive() {
+  install -d artifacts
+  tar -cvzf artifacts/sightmachine-documentation-${_GIT_SHA}.tar.gz -C _book .
+}
+
+##
+## Main
+##
 cd ${_SCRIPT_DIR}/..
-test -d artifact && rm -rf artifact
-test -d _book && rm -rf _book
-sudo docker-compose run --rm gitbook gitbook build
-sudo chown -R jenkins:jenkins _book #OMG
-install -d artifact
-tar -cvzf artifact/sightmachine-documentation-${_GIT_SHA}.tar.gz -C _book .
+_create_temporary_dirs
+_build
+_archive
 cd -
---
+++ jenkins/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+##
+## Deploy tar.gz based artifact to remote web server
+##
+
+set -x
+set -e
+
+
+true
